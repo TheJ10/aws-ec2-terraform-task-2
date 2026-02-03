dc4afe658f768fa4aa36294344f090c5641d9f20
Jaspal Gundla <jaspalgundla@gmail.com>
2026-02-03 15:21:51 +0530

    Provision EC2 instance using Terraform

--- 03-ec2-terraform-deployment/main.tf
+++ 03-ec2-terraform-deployment/main.tf
@@ -1,8 +1,44 @@
-resource "aws_instance" "ec2_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
 
-  tags = {
-    Name = var.instance_name
+resource "aws_security_group" "web_sg" {
+  name_prefix = "web-sg-"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  # Inbound rule for SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound rule for HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    # Allow HTTP traffic from anywhere
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rule 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+#instance creation
+resource "aws_instance" "instance1" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  key_name = var.key_name
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+ }
--- 03-ec2-terraform-deployment/variables.tf
+++ 03-ec2-terraform-deployment/variables.tf
@@ -1,16 +1,23 @@
-variable "instance_name" {
-  description = "Name tag for EC2 instance"
-  type        = string
-  default     = "terraform-ec2-instance"
+variable "aws_region" {
+    type = string
+    default = "ap-south-1"
 }
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
+variable "project" {
+  type = string
+  default = "terraform-ec2"
 }
 
 variable "ami_id" {
-  description = "ami-0861f4e788f5069dd"
-  type        = string
+  type = string
+  default = "ami-0861f4e788f5069dd"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  type = string
+  default = "keys"
 }
